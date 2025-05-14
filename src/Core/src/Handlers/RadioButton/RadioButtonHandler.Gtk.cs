d798cef473435c0d9aed57ac8aeeded6b093b2dc
Lemon73 <108001913+Lemon73-Computing@users.noreply.github.com>
2025-05-14 20:21:22 +0900

    Update RadioButtonHandler.Gtk.cs

--- src/Core/src/Handlers/RadioButton/RadioButtonHandler.Gtk.cs
+++ src/Core/src/Handlers/RadioButton/RadioButtonHandler.Gtk.cs
@@ -7,20 +7,27 @@ namespace Microsoft.Maui.Handlers
 	{
 		protected override RadioButton CreatePlatformView()
   		{
-            var model = new Label(typeof(string));
-			var rb = new RadioButton(model);
-
-            return rb;
+ 			// Note: We set a random GUID as the GroupName as part of the work-around in https://github.com/dotnet/maui/issues/11418
+			return new RadioButton(Guid.NewGuid().ToString());
 	 	}
 
+		[MissingMapper]
 		public static void MapBackground(IRadioButtonHandler handler, IRadioButton radioButton) { }
+		[MissingMapper]
 		public static void MapIsChecked(IRadioButtonHandler handler, IRadioButton radioButton) { }
+		[MissingMapper]
 		public static void MapContent(IRadioButtonHandler handler, IRadioButton radioButton) { }
+		[MissingMapper]
 		public static void MapTextColor(IRadioButtonHandler handler, ITextStyle textStyle) { }
+		[MissingMapper]
 		public static void MapCharacterSpacing(IRadioButtonHandler handler, ITextStyle textStyle) { }
+		[MissingMapper]
 		public static void MapFont(IRadioButtonHandler handler, ITextStyle textStyle) { }
+		[MissingMapper]
 		public static void MapStrokeColor(IRadioButtonHandler handler, IRadioButton radioButton) { }
+		[MissingMapper]
 		public static void MapStrokeThickness(IRadioButtonHandler handler, IRadioButton radioButton) { }
+		[MissingMapper]
 		public static void MapCornerRadius(IRadioButtonHandler handler, IRadioButton radioButton) { }
 	}
 }
